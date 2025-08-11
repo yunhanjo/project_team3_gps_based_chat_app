bcfdc2475169b53b7f53c8342c0723da512d02be
hyun471 <khs101ttl@gmail.com>
2025-08-11 17:03:24 +0900

    ✨ splash 페이지 추가

--- lib/main.dart
+++ lib/main.dart
@@ -3,7 +3,7 @@ import 'package:google_fonts/google_fonts.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:project_team3_gps_based_chat_app/firebase_options.dart';
-import 'package:project_team3_gps_based_chat_app/pages/welcome/welcome_page.dart';
+import 'package:project_team3_gps_based_chat_app/pages/splash/splash_page.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -19,7 +19,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: WelcomePage(),
+      home: SplashPage(),
       theme: ThemeData(
         textTheme: GoogleFonts.playpenSansTextTheme(),
       ),
--- lib/pages/splash/splash_page.dart
+++ lib/pages/splash/splash_page.dart
@@ -1,8 +1,28 @@
 import 'package:flutter/material.dart';
+import 'package:project_team3_gps_based_chat_app/pages/welcome/welcome_page.dart';
 
-class SplashPage extends StatelessWidget {
+class SplashPage extends StatefulWidget {
+  @override
+  State<SplashPage> createState() => _SplashPageState();
+}
+
+class _SplashPageState extends State<SplashPage> {
+  @override
+  void initState() {
+    super.initState();
+
+    Future.delayed(Duration(seconds: 5), () {
+      Navigator.pushReplacement(
+        context,
+        MaterialPageRoute(builder: (context) => WelcomePage()),
+      );
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(body: Text('data'));
+    return Scaffold(
+      body: Center(child: Image.asset('assets/icons/logo.png')),
+    );
   }
 }
