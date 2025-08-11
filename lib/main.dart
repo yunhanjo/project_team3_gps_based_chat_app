6e70c23c256618698589acdd1021780ec9d9e7c6
hyun471 <khs101ttl@gmail.com>
2025-08-11 15:16:12 +0900

    📝 main text 변수 제거

--- lib/main.dart
+++ lib/main.dart
@@ -5,23 +5,6 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:project_team3_gps_based_chat_app/firebase_options.dart';
 import 'package:project_team3_gps_based_chat_app/pages/welcome/welcome_page.dart';
 
-// String chatID;
-// String chatNM;
-// String sender;
-// String address;
-// String message;
-// DateTime createdAt;
-
-ChatRoomInfo aa = ChatRoomInfo(
-  chatID: 'aa1234',
-  name: '조민우',
-  address: '상암동',
-  chatNM: '초보 등산',
-  category: '운동',
-  descript: '등산할 사람~',
-  hostkey: 'aaa',
-);
-
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(
