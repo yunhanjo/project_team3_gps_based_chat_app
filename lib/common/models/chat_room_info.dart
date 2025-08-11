998e9cfa7a82fb8ea43bcad76c70603592b6c850
chori0204 <chori0204@gmail.com>
2025-08-11 21:02:00 +0900

    ✨ 채팅방 리스트 조회 화면 추가

--- lib/common/models/chat_room_info.dart
+++ lib/common/models/chat_room_info.dart
@@ -1,11 +1,15 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+
 class ChatRoomInfo {
-  String chatID;
-  String name;
-  String address;
-  String chatNM;
-  String category;
-  String descript;
-  String hostkey;
+  final String chatID;
+  final String name;
+  final String address;
+  final String chatNM;
+  final String category;
+  final String descript;
+  final String hostkey;
+  final DateTime createdAt;  
+
   ChatRoomInfo({
     required this.chatID,
     required this.name,
@@ -14,18 +18,21 @@ class ChatRoomInfo {
     required this.category,
     required this.descript,
     required this.hostkey,
+    required this.createdAt
   });
 
   ChatRoomInfo.fromJson(Map<String, dynamic> map, String id)
     : this(
         chatID: id,
-        name: map['name'],
-        address: map['address'],
-        chatNM: map['chatNM'],
-        category: map['category'],
-        descript: map['descript'],
-        hostkey: map['hostkey'],
+        name: map['name'] as String? ?? '',
+        address: map['address'] as String? ?? '',
+        chatNM: map['chatNM'] as String? ?? '',
+        category: map['category'] as String? ?? '',
+        descript: map['descript'] as String? ?? '',
+        hostkey: map['hostkey'] as String? ?? '',
+        createdAt: (map['createdAt'] as Timestamp?)?.toDate() ?? DateTime(2000)
       );
+
 
   Map<String, dynamic> toJson() {
     return {
@@ -35,6 +42,7 @@ class ChatRoomInfo {
       'category': category,
       'descript': descript,
       'hostkey': hostkey,
+      'createdAt': Timestamp.fromDate(createdAt),
     };
   }
 }
